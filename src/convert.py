1a7620afc82b0eb0c038ad4858938dc863e0a81e
peixed351 <peixed351@gmail.com>
2024-03-01 19:08:34 +0300

    add settings

--- src/convert.py
+++ src/convert.py
@@ -1,7 +1,12 @@
+import csv
+import glob
 import os
 import shutil
 
+import numpy as np
 import supervisely as sly
+from cv2 import connectedComponents
+from dataset_tools.convert import unpack_if_archive
 from supervisely.io.fs import (
     file_exists,
     get_file_name,
@@ -11,7 +16,6 @@ from supervisely.io.fs import (
 from tqdm import tqdm
 
 import src.settings as s
-from dataset_tools.convert import unpack_if_archive
 
 
 def convert_and_upload_supervisely_project(
@@ -19,72 +23,109 @@ def convert_and_upload_supervisely_project(
 ) -> sly.ProjectInfo:
     # Possible structure for bbox case. Feel free to modify as you needs.
 
-    root_path = ""
-    images_folder = "images"
-    bboxes_folder = "labels"
-    batch_size = 30
-    img_ext = ".png"
-    ann_ext = ".txt"
+    dataset_path = "/home/alex/DATASETS/TODO/MP-IDB"
+    images_folder = "img"
+    anns_folder = "gt"
+    bboxes_path = "/home/alex/DATASETS/TODO/MP-IDB/Falciparum/mp-idb-falciparum.csv"
+    batch_size = 5
+    ds_name = "ds"
+
+    def fix_masks(image_np: np.ndarray) -> np.ndarray:
+        lower_bound = np.array([70, 110, 0])
+        upper_bound = np.array([255, 255, 255])
+        condition_white = np.logical_and(
+            np.all(image_np >= lower_bound, axis=2), np.all(image_np <= upper_bound, axis=2)
+        )
+
+        lower_bound = np.array([0, 0, 0])
+        upper_bound = np.array([20, 20, 20])
+        condition_black = np.logical_and(
+            np.all(image_np >= lower_bound, axis=2), np.all(image_np <= upper_bound, axis=2)
+        )
+
+        image_np[np.where(condition_white)] = (255, 255, 255)
+        image_np[np.where(condition_black)] = (0, 0, 0)
+
+        return image_np
 
     def create_ann(image_path):
-        labels, img_tags, label_tags = [], [], []
+        labels = []
+        tags = []
 
-        image_np = sly.imaging.image.read(image_path)[:, :, 0]
-        img_height = image_np.shape[0]
-        img_width = image_np.shape[1]
+        # image_np = sly.imaging.image.read(image_path)[:, :, 0]
+        img_height = 1944  # image_np.shape[0]
+        img_wight = 2592  #  image_np.shape[1]
 
-        file_name = get_file_name(image_path)
-        curr_anns_dirpath = ""
-        ann_path = os.path.join(curr_anns_dirpath, file_name + ann_ext)
+        obj_class_name = image_path.split("/")[-3]
+        obj_class = meta.get_obj_class(obj_class_name.lower())
 
-        if file_exists(ann_path):
-            with open(ann_path) as f:
-                content = f.read().split("\n")
-                for curr_data in content:
-                    if len(curr_data) != 0:
-                        curr_data = list(map(float, curr_data.split(" ")))
+        im_name = get_file_name(image_path)
+        tags_data = im_name.split("-")[-1].split("_")
+        for tag_data in tags_data:
+            curr_meta = letter_to_meta[tag_data]
+            tag = sly.Tag(curr_meta)
+            tags.append(tag)
 
-                        left = int((curr_data[1] - curr_data[3] / 2) * img_width)
-                        right = int((curr_data[1] + curr_data[3] / 2) * img_width)
-                        top = int((curr_data[2] - curr_data[4] / 2) * img_height)
-                        bottom = int((curr_data[2] + curr_data[4] / 2) * img_height)
+        mask_path = image_path.replace("img", "gt")
 
-                        rectangle = sly.Rectangle(top=top, left=left, bottom=bottom, right=right)
+        if file_exists(mask_path):
+            # mask_np = sly.imaging.image.read(mask_path)[:, :, 0]
+            mask_np = sly.imaging.image.read(mask_path)[:, :, :]
+            mask_np = fix_masks(mask_np)[:, :, 0]
+            mask = mask_np == 255
+            ret, curr_mask = connectedComponents(mask.astype("uint8"), connectivity=8)
+            for i in range(1, ret):
+                obj_mask = curr_mask == i
+                curr_bitmap = sly.Bitmap(obj_mask)
+                curr_label = sly.Label(curr_bitmap, obj_class)
+                labels.append(curr_label)
 
-                        for obj_class in obj_classes:
-                            if obj_class.name == idx2clsname[curr_data[0]]:
-                                curr_obj_class = obj_class
-                                break
-                        label = sly.Label(rectangle, curr_obj_class, label_tags)
-                        labels.append(label)
+        return sly.Annotation(img_size=(img_height, img_wight), labels=labels, img_tags=tags)
 
-        return sly.Annotation(img_size=(img_height, img_width), labels=labels, img_tags=img_tags)
+    falciparum = sly.ObjClass("falciparum", sly.AnyGeometry)
+    malariae = sly.ObjClass("malariae", sly.Bitmap)
+    ovale = sly.ObjClass("ovale", sly.Bitmap)
+    vivax = sly.ObjClass("vivax", sly.Bitmap)
 
-    class_names = ["class1", "class2", ...]
-    idx2clsname = {}
-    obj_classes = [sly.ObjClass(name, sly.Rectangle) for name in class_names]
+    ring_stage_meta = sly.TagMeta("ring stage", sly.TagValueType.NONE)
+    trophozoite_stage_meta = sly.TagMeta("trophozoite stage", sly.TagValueType.NONE)
+    schizont_stage_meta = sly.TagMeta("schizont stage", sly.TagValueType.NONE)
+    gametocyte_stage_meta = sly.TagMeta("gametocyte stage", sly.TagValueType.NONE)
+
+    letter_to_meta = {
+        "R": ring_stage_meta,
+        "T": trophozoite_stage_meta,
+        "S": schizont_stage_meta,
+        "G": gametocyte_stage_meta,
+    }
 
     project = api.project.create(workspace_id, project_name, change_name_if_conflict=True)
-    meta = sly.ProjectMeta(obj_classes=obj_classes)
+    meta = sly.ProjectMeta(
+        obj_classes=[falciparum, malariae, ovale, vivax],
+        tag_metas=[
+            ring_stage_meta,
+            trophozoite_stage_meta,
+            schizont_stage_meta,
+            gametocyte_stage_meta,
+        ],
+    )
     api.project.update_meta(project.id, meta.to_json())
 
-    for ds_name in os.listdir(root_path):
-        dataset = api.dataset.create(project.id, ds_name, change_name_if_conflict=True)
-        dataset_path = os.path.join(root_path, ds_name)
+    dataset = api.dataset.create(project.id, ds_name, change_name_if_conflict=True)
 
-        images_pathes = sly.fs.list_files_recursively(dataset_path, valid_extensions=[img_ext])
+    images_pathes = glob.glob(dataset_path + "/*/img/*.jpg")
 
-        pbar = tqdm(desc=f"Create dataset '{ds_name}'", total=len(images_pathes))
-        for images_pathes_batch in sly.batched(images_pathes, batch_size=batch_size):
-            images_names_batch = [
-                get_file_name_with_ext(image_path) for image_path in images_pathes_batch
-            ]
+    progress = sly.Progress("Create dataset {}".format(ds_name), len(images_pathes))
 
-            img_infos = api.image.upload_paths(dataset.id, images_names_batch, images_pathes_batch)
-            img_ids = [image.id for image in img_infos]
+    for img_pathes_batch in sly.batched(images_pathes, batch_size=batch_size):
+        img_names_batch = [get_file_name_with_ext(im_path) for im_path in img_pathes_batch]
 
-            anns = [create_ann(image_path) for image_path in images_pathes_batch]
-            api.annotation.upload_anns(img_ids, anns)
+        img_infos = api.image.upload_paths(dataset.id, img_names_batch, img_pathes_batch)
+        img_ids = [im_info.id for im_info in img_infos]
 
-            pbar.update(len(images_names_batch))
+        anns = [create_ann(image_path) for image_path in img_pathes_batch]
+        api.annotation.upload_anns(img_ids, anns)
+
+        progress.iters_done_report(len(img_names_batch))
+
     return project
--- src/settings.py
+++ src/settings.py
@@ -13,37 +13,47 @@ from dataset_tools.templates import (
 ##################################
 # * Before uploading to instance #
 ##################################
-PROJECT_NAME: str = None
-PROJECT_NAME_FULL: str = None
+PROJECT_NAME: str = "MP IDB"
+PROJECT_NAME_FULL: str = "MP-IDB: Malaria Parasite Image Dataset"
 HIDE_DATASET = True  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
 ##################################
-LICENSE: License = None
-APPLICATIONS: List[Union[Industry, Domain, Research]] = None
-CATEGORY: Category = None
+LICENSE: License = License.MIT(
+    source_url="https://github.com/andrealoddo/MP-IDB-The-Malaria-Parasite-Image-Database-for-Image-Processing-and-Analysis/blob/master/LICENSE"
+)
+APPLICATIONS: List[Union[Industry, Domain, Research]] = [Industry.Medical()]
+CATEGORY: Category = Category.Medical()
 
-CV_TASKS: List[CVTask] = None
-ANNOTATION_TYPES: List[AnnotationType] = None
+CV_TASKS: List[CVTask] = [
+    CVTask.InstanceSegmentation(),
+    CVTask.SemanticSegmentation(),
+    CVTask.ObjectDetection(),
+]
+ANNOTATION_TYPES: List[AnnotationType] = [AnnotationType.InstanceSegmentation()]
 
 RELEASE_DATE: Optional[str] = None  # e.g. "YYYY-MM-DD"
 if RELEASE_DATE is None:
-    RELEASE_YEAR: int = None
+    RELEASE_YEAR: int = 2019
 
-HOMEPAGE_URL: str = None
+HOMEPAGE_URL: str = (
+    "https://github.com/andrealoddo/MP-IDB-The-Malaria-Parasite-Image-Database-for-Image-Processing-and-Analysis#mp-idb-the-malaria-parasite-image-database-for-image-processing-and-analysis"
+)
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = None
+PREVIEW_IMAGE_ID: int = 14440967
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
-GITHUB_URL: str = None
+GITHUB_URL: str = "https://github.com/dataset-ninja/mp-idb"
 # URL to GitHub repo on dataset ninja (e.g. "https://github.com/dataset-ninja/some-dataset")
 
 ##################################
 ### * Optional after uploading ###
 ##################################
-DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = None
+DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = (
+    "https://github.com/andrealoddo/MP-IDB-The-Malaria-Parasite-Image-Database-for-Image-Processing-and-Analysis#mp-idb-the-malaria-parasite-image-database-for-image-processing-and-analysis"
+)
 # Optional link for downloading original dataset (e.g. "https://some.com/dataset/download")
 
 CLASS2COLOR: Optional[Dict[str, List[str]] | Literal["predefined"]] = "predefined"
@@ -51,21 +61,41 @@ CLASS2COLOR: Optional[Dict[str, List[str]] | Literal["predefined"]] = "predefine
 
 # If you have more than the one paper, put the most relatable link as the first element of the list
 # Use dict key to specify name for a button
-PAPER: Optional[Union[str, List[str], Dict[str, str]]] = None
+PAPER: Optional[Union[str, List[str], Dict[str, str]]] = (
+    "https://www.researchgate.net/publication/331570908_MP-IDB_The_Malaria_Parasite_Image_Database_for_Image_Processing_and_Analysis"
+)
 BLOGPOST: Optional[Union[str, List[str], Dict[str, str]]] = None
-REPOSITORY: Optional[Union[str, List[str], Dict[str, str]]] = {
-    "GitHub": "some_link_to_repo_if_exists"
-}
+REPOSITORY: Optional[Union[str, List[str], Dict[str, str]]] = None
 
 CITATION_URL: Optional[str] = None
-AUTHORS: Optional[List[str]] = None
-AUTHORS_CONTACTS: Optional[List[str]] = None
+AUTHORS: Optional[List[str]] = [
+    "Andrea Loddo",
+    "Cecilia Di Ruberto",
+    "Michel Kocher",
+    "Guy Prod Hom",
+]
+AUTHORS_CONTACTS: Optional[List[str]] = [
+    "andrea.loddo@unica.it",
+    "dirubert@unica.it",
+    "michel.kocher@heig-vd.ch",
+    "guy.prodhom@chuv.ch",
+]
 
-ORGANIZATION_NAME: Optional[Union[str, List[str]]] = None
-ORGANIZATION_URL: Optional[Union[str, List[str]]] = None
+ORGANIZATION_NAME: Optional[Union[str, List[str]]] = [
+    "University of Cagliari, Italy",
+    "École Polytechnique Fédérale de Lausanne (EPFL), Switzerland",
+    "University of Lausanne and University Hospital Center, Switzerland",
+]
+ORGANIZATION_URL: Optional[Union[str, List[str]]] = [
+    "https://web.unica.it/",
+    "https://www.epfl.ch/",
+    "https://www.lausanneuniversityhospital.com/home",
+]
 
 # Set '__PRETEXT__' or '__POSTTEXT__' as a key with string value to add custom text. e.g. SLYTAGSPLIT = {'__POSTTEXT__':'some text}
-SLYTAGSPLIT: Optional[Dict[str, Union[List[str], str]]] = None
+SLYTAGSPLIT: Optional[Dict[str, Union[List[str], str]]] = {
+    "parasites": ["ring stage", "trophozoite stage", "schizont stage", "gametocyte stage"]
+}
 TAGS: Optional[List[str]] = None
 
 
